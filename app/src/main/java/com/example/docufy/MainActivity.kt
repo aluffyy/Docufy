b0308948fd5d9645e247eb1a4ae9ce1ac2a25bce
Mustakim Islam Alif <alifisalu@gmail.com>
2024-03-17 01:23:02 +0600

    brrrrrrr

--- app/src/main/java/com/example/docufy/MainActivity.kt
+++ app/src/main/java/com/example/docufy/MainActivity.kt
@@ -31,37 +31,46 @@ import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Configure options for the document scanner
         val options = GmsDocumentScannerOptions.Builder()
             .setScannerMode(SCANNER_MODE_FULL)
             .setGalleryImportAllowed(true)
-            .setPageLimit(20)
+            .setPageLimit(30)
             .setResultFormats(RESULT_FORMAT_JPEG, RESULT_FORMAT_PDF)
             .build()
+        // Get an instance of the ML Kit document scanner client
         val scanner = GmsDocumentScanning.getClient(options)
 
-
+        // Implement the user interface (UI) using Jetpack Compose
         setContent {
             DocufyTheme {
+                // Implement the user interface (UI) using Jetpack Compose
+                // Surface is the root container for the UI elements
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    // Remember a mutable list to store scanned document URIs
                     val imageUris = remember {
                         mutableStateListOf<Uri>()
                     }
+                    // Create an ActivityResultLauncher to handle the scan results
                     val scannerLauncher = rememberLauncherForActivityResult(
                         contract = ActivityResultContracts.StartIntentSenderForResult(),
                         onResult = {
 
                         }
                     )
+
+                    // Column layout to arrange elements vertically
                     Column(
                         modifier = Modifier
                             .fillMaxSize(),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
+                        // Display scanned documents using AsyncImage from Coil library
                         imageUris.forEach { uri ->
                             AsyncImage(
                                 model = uri,
@@ -70,14 +79,16 @@ class MainActivity : ComponentActivity() {
                                 modifier = Modifier.fillMaxSize()
                             )
                         }
+                        // Button to initiate the document scanning process
                         Button(onClick = {
+                            // Get the intent to start the document scanner UI
                             scanner.getStartScanIntent(this@MainActivity)
-                                .addOnSuccessListener {
+                                .addOnSuccessListener {// Handle successful intent retrieval
                                     scannerLauncher.launch(
                                         IntentSenderRequest.Builder(it).build()
                                     )
                                 }
-                                .addOnFailureListener {
+                                .addOnFailureListener {// Handle errors during intent retrieval
                                     Toast.makeText(
                                         applicationContext,
                                         it.message,
@@ -85,7 +96,7 @@ class MainActivity : ComponentActivity() {
                                     ).show()
                                 }
                         }) {
-                            Text(text = "Scan it out")
+                            Text(text = "Docufy papers")
                         }
                     }
                 }
